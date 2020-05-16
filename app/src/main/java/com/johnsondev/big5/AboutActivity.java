ebf6044b3ebd81f396b6590e70e7e0b8dd982a88
JohnsonWedDev <57323391+JohnsonWedDev@users.noreply.github.com>
2020-05-16 14:51:46 +0300

    Commit 6-Добавлено TestLayout

--- app/src/main/java/com/johnsondev/big5/AboutActivity.java
+++ app/src/main/java/com/johnsondev/big5/AboutActivity.java
@@ -3,7 +3,10 @@ package com.johnsondev.big5;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,14 +19,21 @@ import com.google.firebase.database.ValueEventListener;
 
 public class AboutActivity extends AppCompatActivity {
 
-
+    private Button nextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+        nextBtn = findViewById(R.id.next_btn);
 
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AboutActivity.this, TestActivity.class));
+            }
+        });
 
 
     }
