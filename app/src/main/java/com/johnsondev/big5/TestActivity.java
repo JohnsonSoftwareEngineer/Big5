384d5731e1627427ce7cd8a88dd9d79781bb9187
JohnsonWedDev <57323391+JohnsonWedDev@users.noreply.github.com>
2020-05-17 18:23:56 +0300

    Commit 7-Добавлено Activity для Тестов

--- app/src/main/java/com/johnsondev/big5/TestActivity.java
+++ app/src/main/java/com/johnsondev/big5/TestActivity.java
@@ -3,6 +3,9 @@ package com.johnsondev.big5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+//                        change textView
+//        String text = String.valueOf(Integer.parseInt(textView.getText().toString())+1);
+//        textView.setText(text);
+
+
     }
 }
